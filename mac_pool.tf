b44a2c3e3a8931cba4c53650ca9b205607257c43
student <tfdc-student@flintmail.com>
2022-04-01 11:50:12 +0000

    Dynamic MAC blocks

--- mac_pool.tf
+++ mac_pool.tf
@@ -12,7 +12,7 @@ resource "intersight_macpool_pool" "PodXX_MAC_BLOCKS" {
   dynamic "mac_blocks" {
     for_each = formatlist("%d", range(1, 3))
     content {
-      from = "XX:25:B5:AF:1${mac_blocks.value}:00"
+      from = "00:25:B5:AF:1${mac_blocks.value}:00"
       size = 10
     }
   }
