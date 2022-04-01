68742316c59cbfe130eebe6066a7a36cd21a3595
student <tfdc-student@flintmail.com>
2022-04-01 13:35:34 +0000

    Simple MAC pool

--- mac_pool.tf
+++ mac_pool.tf
@@ -1,14 +1,20 @@
-/*
 resource "intersight_macpool_pool" "PodXX_MAC_POOL" {  
-  count = 2
-  name = "PodXX_MAC_POOL_${count.index}"
+  name = "PodXX_MAC_POOL"
 
   mac_blocks {
-    from = "XX:25:B5:AF:10:${count.index}0"
-    to = "XX:25:B5:AF:10:${count.index}A"
+    from = "XX:00:00:00:00:00"
+    size = 1000
   }
 }
 
+
+
+
+
+
+
+
+/*
 resource "intersight_macpool_pool" "PodXX_MAC_BLOCKS" {
   name = "PodXX_MAC_BLOCKS"
   dynamic "mac_blocks" {
