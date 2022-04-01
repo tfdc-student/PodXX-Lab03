bdd0d286b745e2fbdbaae41cca50084d600a7708
student <tfdc-student@flintmail.com>
2022-04-01 15:55:28 +0000

    for_each meta argument

--- mac_pool.tf
+++ mac_pool.tf
@@ -7,3 +7,16 @@ resource "intersight_macpool_pool" "PodXX_MAC_POOL" {
     size = 1000
   }
 }
+
+resource "intersight_macpool_pool" "PodXX_MAC_POOL_FOREACH" {  
+  for_each = {
+      FIRST = "A"
+      SECOND = "B"
+  }
+  name = "PodXX_MAC_POOL_${each.key}"
+
+  mac_blocks {
+    from = "00:0${each.value}:00:00:00:00"
+    size = 1000
+  }
+}
