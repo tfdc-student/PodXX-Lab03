5bc121e979785c23bf09b2047d886e4685ec6bc9
student <tfdc-student@flintmail.com>
2022-04-01 13:51:58 +0000

    XX

--- mac_pool.tf
+++ mac_pool.tf
@@ -2,7 +2,7 @@ resource "intersight_macpool_pool" "PodXX_MAC_POOL" {
   name = "PodXX_MAC_POOL"
 
   mac_blocks {
-    from = "00:00:00:00:00:00"
+    from = "XX:00:00:00:00:00"
     size = 1000
   }
 }
