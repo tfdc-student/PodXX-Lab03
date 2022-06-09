2db33e19a4b2c8c3eb14e92bc9a799d3fdd02fdc
student <tfdc-studentflintmail.com>
2022-06-09 07:20:15 +0000

    Simple MAC pool

--- mac_pool.tf
+++ mac_pool.tf
@@ -1,6 +1,5 @@
 resource "intersight_macpool_pool" "PodXX_MAC_POOL" {  
-  count = 2
-  name = "PodXX_MAC_POOL_${count.index}"
+  name = "PodXX_MAC_POOL"
 
   mac_blocks {
     from = "00:03:XX:00:00:00"
